21df5b5f99fcabd9d4a767bda5a2062efba342a5
Andrew Charneski <acharneski@gmail.com>
2012-07-19 10:43:41 -0700

    Fixed android shared object build

--- android-project/jni/Application.mk
+++ android-project/jni/Application.mk
@@ -1,6 +1,6 @@
 MY_LOCAL_PATH := $(call my-dir)
 #APP_MODULES := speex libavutil libavcodec libavformat libswresample libswscale libpostproc 
-APP_MODULES := speex libavutil libavcodec libavformat libswresample libswscale libpostproc libavfilter 
+APP_MODULES := speex libavutil libavcodec libavformat libswresample libswscale libpostproc libavfilter libavresample
 APP_OPTIM := release 
 APP_ABI := armeabi armeabi-v7a
 
--- android-project/jni/speex/Android.mk
+++ android-project/jni/speex/Android.mk
@@ -3,18 +3,15 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := speex
 
 LOCAL_SRC_FILES := \
-	libspeex/testenc_uwb.c \
 	libspeex/hexc_10_32_table.c \
 	libspeex/vbr.c \
 	libspeex/scal.c \
-	libspeex/testdenoise.c \
 	libspeex/sb_celp.c \
 	libspeex/vorbis_psy.c \
 	libspeex/high_lsp_tables.c \
 	libspeex/lsp.c \
 	libspeex/filters.c \
 	libspeex/exc_20_32_table.c \
-	libspeex/testenc.c \
 	libspeex/speex_header.c \
 	libspeex/speex_callbacks.c \
 	libspeex/bits.c \
@@ -26,6 +23,7 @@ LOCAL_SRC_FILES := \
 	libspeex/modes_wb.c \
 	libspeex/hexc_table.c \
 	libspeex/kiss_fftr.c \
+	libspeex/fftwrap.c \
 	libspeex/gain_table_lbr.c \
 	libspeex/exc_10_16_table.c \
 	libspeex/jitter.c \
@@ -33,9 +31,6 @@ LOCAL_SRC_FILES := \
 	libspeex/mdf.c \
 	libspeex/window.c \
 	libspeex/exc_5_256_table.c \
-	libspeex/testjitter.c \
-	libspeex/testresample.c \
-	libspeex/testenc_wb.c \
 	libspeex/smallft.c \
 	libspeex/preprocess.c \
 	libspeex/nb_celp.c \
@@ -44,7 +39,6 @@ LOCAL_SRC_FILES := \
 	libspeex/speex.c \
 	libspeex/modes.c \
 	libspeex/stereo.c \
-	libspeex/testecho.c \
 	libspeex/buffer.c \
 	libspeex/cb_search.c \
 	libspeex/gain_table.c \
@@ -53,8 +47,11 @@ LOCAL_SRC_FILES := \
 	libspeex/vq.c
 
 LOCAL_C_INCLUDES += include
-LOCAL_CFLAGS = -DFIXED_POINT -DEXPORT="" -UHAVE_CONFIG_H -I$(LOCAL_PATH)/include
-include $(BUILD_STATIC_LIBRARY)
-include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := -DFIXED_POINT -DEXPORT="" -UHAVE_CONFIG_H -I$(LOCAL_PATH)/include
+#LOCAL_CFLAGS += -DUSE_SMALLFT
+LOCAL_CFLAGS += -DUSE_KISS_FFT
+
+include $(BUILD_SHARED_LIBRARY)
 
 
