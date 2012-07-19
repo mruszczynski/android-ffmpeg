de9e0d53951f3fbdc6197d5da96e9f5e8d2bf544
Andrew Charneski <acharneski@gmail.com>
2012-07-19 00:51:10 -0700

    Android build now creates shared objects.

--- android-project/jni/Application.mk
+++ android-project/jni/Application.mk
@@ -1,6 +1,6 @@
 MY_LOCAL_PATH := $(call my-dir)
-APP_MODULES := ffmpeg speex
-#APP_MODULES := speex ffmpeg
+#APP_MODULES := speex libavutil libavcodec libavformat libswresample libswscale libpostproc 
+APP_MODULES := speex libavutil libavcodec libavformat libswresample libswscale libpostproc libavfilter 
 APP_OPTIM := release 
 APP_ABI := armeabi armeabi-v7a
 
